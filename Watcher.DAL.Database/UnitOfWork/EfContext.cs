e855d46a33763f8cd43087490017f1621ba9929f
Liavontsi Khachuyeu <liavontsi_khachuyeu@epam.com>
2022-07-29 01:03:09 +0300

    Update backend password

--- Watcher.DAL.Database/UnitOfWork/EfContext.cs
+++ Watcher.DAL.Database/UnitOfWork/EfContext.cs
@@ -53,7 +53,7 @@ namespace Watcher.DAL.Database.UnitOfWork
 			optionsBuilder.UseMySql("server=localhost;port=3308;database=watcher;UserId=root;Password=Boost2017;CharSet=utf8", serverVersion);
 #else
 			var serverAddress = Environment.GetEnvironmentVariable("DATABASE_SERVER_ADDRESS");
-			optionsBuilder.UseMySql($"server={serverAddress};database=watcher;UserId=root;Password=Boost2017;CharSet=utf8", serverVersion);
+			optionsBuilder.UseMySql($"server={serverAddress};database=watcher;UserId=root;Password=grippolek2022;CharSet=utf8", serverVersion);
 #endif
 		}
 	}

1370eaaf19a85a4bd9edff883bc80677294d6286
Liavontsi Khachuyeu <liavontsi_khachuyeu@epam.com>
2022-07-28 22:44:13 +0300

    Improve getting dependencies

---
+++ scripts/get_all.sh
@@ -0,0 +1,16 @@
+cd Watcher/client_app
+
+fvm flutter pub get
+
+cd packages
+
+cd taskus
+fvm flutter pub get
+
+cd .. && cd watcher_client_bll
+fvm flutter pub get
+
+cd .. && cd watcher_client_dal
+fvm flutter pub get
+
+cd ../../..
